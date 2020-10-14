2feaaa68b3819150d84d08e33d6d3784b7a262a3
Ian k Armstrong <ian.k.armstrong@gmail.com>
2020-10-14 14:46:16 +0200

    14.10.2020

--- createProject.sh
+++ createProject.sh
@@ -1,5 +1,4 @@
-#!/bin/bash 
-#
+#!/bin/bash #
 # Create an Android Java App from the command line
 #
 
@@ -84,7 +83,7 @@ echo "buildscript {
         jcenter()
     }
     dependencies {
-        classpath \"com.android.tools.build:gradle:4.0.1\"
+        classpath \"com.android.tools.build:gradle:4.1.0\"
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -137,7 +136,7 @@ android {
 
 dependencies {
     implementation 'androidx.appcompat:appcompat:1.2.0'
-    implementation 'androidx.constraintlayout:constraintlayout:2.0.1'
+    implementation 'androidx.constraintlayout:constraintlayout:2.0.2'
 }" > app/build.gradle
 fi
 
